bde9d094f0d3c6ae25326b041528c569ec693f73
ryan98100 <c14180115@john.petra.ac.id>
2020-04-22 08:27:42 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import{Routes ,RouterModule} from '@angular/router'
+const Routes:Routes = [
+  {path:'/home' , component:HomeComponent}
+]
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(Routes)],
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
